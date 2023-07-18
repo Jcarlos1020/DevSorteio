08823b5869dfbf43c67d4582434641de791e7070
Jcarlos1020 <mprojetos14@gmail.com>
2023-07-18 19:16:57 -0300

    EM FUNCIONAMENTO

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
+
 function numberAleatorio(){
 
-const min = math.ceil(document.querySelector(".input-min").value)
-const max = math.floor(document.querySelector(".input-max").value)
+const min = Math.ceil(document.querySelector(".input-min").value)
+const max = Math.floor(document.querySelector(".input-max").value)
 
 const result= Math.floor(Math.random() * (max - min + 1)) + min;
 
